4f4548c08fc4ad3f7a54466eddf40b18483dcc60
Zen Savona <z@zensavona.com>
2015-10-24 13:14:02 +0200

    test and doc packages

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Naughtygram.Mixfile do
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     test_coverage: [tool: ExCoveralls],
      deps: deps]
   end
 
@@ -14,7 +15,7 @@ defmodule Naughtygram.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger, :httpoison]]
+    [applications: [:logger, :httpoison, :poison]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +28,14 @@ defmodule Naughtygram.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    [{:httpoison, "~> 0.7.4"}]
+    [
+      {:httpoison, "~> 0.7.4"},
+      {:exprintf, "~> 0.1.6"},
+      {:poison, "~> 1.5"},
+      {:exvcr, "~> 0.3", only: [:dev, :test]},
+      {:ex_doc, "~> 0.10.0", only: [:dev, :docs]},
+      {:excoveralls, "~> 0.3", only: [:dev, :test]},
+      {:inch_ex, "~> 0.4.0", only: [:dev, :docs]}
+    ]
   end
 end
