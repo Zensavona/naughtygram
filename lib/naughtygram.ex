b2a84e3b0da46c030ce448f55df3149e94b016e2
Zen Savona <z@zensavona.com>
2015-11-06 11:05:44 +0100

    upload pictures with caption

--- lib/naughtygram.ex
+++ lib/naughtygram.ex
@@ -17,6 +17,7 @@ defmodule Naughtygram do
   created once per user and stored for later use.
   """
   def login_and_return_cookies(username, password, identity) do
+    url = @url <> "/accounts/login/"
     data = Poison.encode!(%{
       username: username,
       password: password,
@@ -33,7 +34,7 @@ defmodule Naughtygram do
     ]
 
     HTTPoison.start
-    resp = HTTPoison.post!(@url<>"/accounts/login/", body, headers)
+    resp = HTTPoison.post!(url, body, headers)
 
     case resp do
       %{status_code: 200} ->
@@ -43,6 +44,7 @@ defmodule Naughtygram do
         cookies = List.insert_at(cookies, -1, {"igfl", username})
         {:ok, cookies}
       _ ->
+        IO.inspect resp
         {:err, "reason"}
     end
   end
@@ -166,4 +168,63 @@ defmodule Naughtygram do
     end
   end
 
+  @doc """
+  Upload a picture from the filesystem
+  """
+  def upload_media(photo, caption, identity, cookies) do
+    url = @url <> "/media/upload/"
+    options = [hackney: [cookie: cookies, follow_redirect: true]]
+    timestamp = to_string(:os.system_time(:seconds))
+
+    headers = [
+      {"User-Agent", identity.user_agent}
+    ]
+
+    HTTPoison.start
+    request = HTTPoison.post!(url, {:multipart, [{"device_timestamp", timestamp}, {:file, photo, { ["form-data"], [name: "\"photo\"", filename: "\"#{photo}\""]},[]}]}, headers, options)
+    response = Poison.decode! request.body
+
+    case response do
+      %{"media_id" => media_id, "status" => "ok"} ->
+        configure(media_id, caption, identity, cookies)
+        # {:ok, media_id}
+      _ ->
+        {:err, "reason"}
+    end
+  end
+
+  defp configure(media_id, caption, identity, cookies) do
+    url = @url <> "/media/configure/"
+
+    options = [hackney: [cookie: cookies, follow_redirect: true]]
+    timestamp = to_string(:os.system_time(:seconds))
+    headers = [
+      {"User-Agent", identity.user_agent},
+      {"Content-Type", "application/x-www-form-urlencoded"}
+    ]
+
+    data = Poison.encode!(%{
+      guid: identity.guid,
+      device_id: "android-#{identity.guid}",
+      device_timestamp: timestamp,
+      media_id: media_id,
+      caption: caption,
+      source_type: "5",
+      filter_type: "0",
+      extra: "{}",
+      "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+    })
+
+    body = Crypto.signed_body(data)
+
+    HTTPoison.start
+    response = Poison.decode! HTTPoison.post!(url, body, headers, options).body
+
+    case response do
+      %{"status" => "ok"} ->
+        {:ok, media_id}
+      _ ->
+        {:err, "reason"}
+    end
+  end
 end
